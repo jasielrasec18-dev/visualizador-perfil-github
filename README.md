8993773440fead8d9506b1d30805f689a39b0281
Jasiel Rasec <jasielrasec18@gmail.com>
2026-03-25 12:49:36 -0300

    feat: adiciona a busca pelos dados do repositório quando o usuário apertar enter

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # visualizador-perfil-github
+
--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
 const profileResults = document.querySelector('.profile-results');
 
-btnSearch.addEventListener('click', async () => {
+async function getUserProfile() {
     const userName = inputSearch.value;
     if (!userName) {
         alert('Por favor, digite um nome de usuário do GitHub.');
@@ -21,5 +21,13 @@ btnSearch.addEventListener('click', async () => {
             console.error('Erro ao buscar o perfil do usuário:', error);
             alert('Usuário não encontrado. Por favor, verifique o nome de usuário e tente novamente.');
             profileResults.innerHTML = "";
+    }
+}
+
+btnSearch.addEventListener('click', getUserProfile);
+
+inputSearch.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        getUserProfile();
     }
 });
